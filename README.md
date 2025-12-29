2c4650f982cd909554d808081ea486a62451c2ba
Steve Taranto <staranto@gmail.com>
2025-12-29 05:55:28 -0500

    Release version 0.3.0

--- README.md
+++ README.md
@@ -59,6 +59,6 @@ plugin "elements-of-style" {
   enabled = true
 
   source  = "github.com/staranto/tflint-ruleset-elements-of-style"
-  version = "0.2.1" # replace as needed
+  version = "0.3.0" # replace as needed
 }
 ```
--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		RuleSet: &tflint.BuiltinRuleSet{
 			Name:    "elements-of-style",
-			Version: "0.2.1",
+			Version: "0.3.0",
 			Rules: []tflint.Rule{
 				comment.NewCommentsRule(),
 				deathmask.NewDeathMaskRule(),
